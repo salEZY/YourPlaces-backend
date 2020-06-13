ac0e870337e393e08ac2628ad18bc14da277e8e8
AleksBobic <bobicaleks@yahoo.com>
2020-06-13 11:50:28 +0200

    add error model

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
   {
-    id: "u1",
+    id: "p1",
     title: "Beograd",
     description: "NAJJACI GRAD",
     location: {
@@ -12,10 +14,10 @@ const DUMMY_PLACES = [
       lng: 20.4868401,
     },
     address: "Djevdjelijska 15",
-    creator: "p1",
+    creator: "u1",
   },
   {
-    id: "u2",
+    id: "p2",
     title: "Beograd",
     description: "GRAD BEO",
     location: {
@@ -23,13 +25,34 @@ const DUMMY_PLACES = [
       lng: 20.495167,
     },
     address: "Bore Prodanovica 9a",
-    creator: "p2",
+    creator: "u2",
   },
 ];
 
+// GET place by place ID
 router.get("/:placeId", (req, res, next) => {
   const placeId = req.params.placeId;
   const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id!", 404);
+  }
+
+  res.json({ place });
+});
+
+// GET place by user ID
+router.get("/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+
+  const place = DUMMY_PLACES.find((place) => place.creator === userId);
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id!", 404)
+    );
+  }
+
   res.json({ place });
 });
 
