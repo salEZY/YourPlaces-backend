d09e40e5a664cc7ddf3fa84f147a9400f10df975
AleksBobic <bobicaleks@yahoo.com>
2020-07-16 20:27:14 +0200

    add connection to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -25,4 +26,13 @@ app.use((error, req, res, next) => {
   res.json({ nessage: error.message || "An unknown error occured!" });
 });
 
-app.listen(5000, () => console.log("Server start at port 5000!"));
+mongoose
+  .connect(
+    "mongodb+srv://salezy:salezy@yourplaces.wnaqk.mongodb.net/db?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("MongoDB connected!");
+    app.listen(5000, () => console.log("Server start at port 5000!"));
+  })
+  .catch((err) => console.log(err));
