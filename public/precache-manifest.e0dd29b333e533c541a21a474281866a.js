758e4c5bf50550f70ee940e32ce39f8ab4f82b97
AleksBobic <bobicaleks@yahoo.com>
2020-08-12 14:05:59 +0200

    add new react app

---
+++ public/precache-manifest.e0dd29b333e533c541a21a474281866a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b734678762b7718ac1f312319dedcad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1413cab46b2fdeb7233c",
+    "url": "/static/css/0.1e632426.chunk.css"
+  },
+  {
+    "revision": "6b4e0559efc36f654097",
+    "url": "/static/css/4.f03fdc5a.chunk.css"
+  },
+  {
+    "revision": "2bd2f4184247f0dc0f17",
+    "url": "/static/css/5.fd5b3f2d.chunk.css"
+  },
+  {
+    "revision": "46acce7fd3726dcc7efc",
+    "url": "/static/css/6.870f495c.chunk.css"
+  },
+  {
+    "revision": "31210923a38454be75f7",
+    "url": "/static/css/7.04bdc6d0.chunk.css"
+  },
+  {
+    "revision": "fbfe1df2042f44a7084b",
+    "url": "/static/css/8.693a6a49.chunk.css"
+  },
+  {
+    "revision": "0bdc4f4f93034b4518f0",
+    "url": "/static/css/main.1d51825b.chunk.css"
+  },
+  {
+    "revision": "1413cab46b2fdeb7233c",
+    "url": "/static/js/0.4a0c5b46.chunk.js"
+  },
+  {
+    "revision": "e0f68c133ce330949662",
+    "url": "/static/js/3.b388317a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.b388317a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4e0559efc36f654097",
+    "url": "/static/js/4.45ac3961.chunk.js"
+  },
+  {
+    "revision": "2bd2f4184247f0dc0f17",
+    "url": "/static/js/5.dde22b37.chunk.js"
+  },
+  {
+    "revision": "46acce7fd3726dcc7efc",
+    "url": "/static/js/6.e27c5648.chunk.js"
+  },
+  {
+    "revision": "31210923a38454be75f7",
+    "url": "/static/js/7.33123ecd.chunk.js"
+  },
+  {
+    "revision": "fbfe1df2042f44a7084b",
+    "url": "/static/js/8.939fcacd.chunk.js"
+  },
+  {
+    "revision": "0bdc4f4f93034b4518f0",
+    "url": "/static/js/main.bb16460a.chunk.js"
+  },
+  {
+    "revision": "5bfaf18ffca873525981",
+    "url": "/static/js/runtime-main.d6e855d4.js"
+  }
+]);
